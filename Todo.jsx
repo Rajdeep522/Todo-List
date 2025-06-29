698e2710327acc3761d71f3e8838c53084b77787
Rajdeep Debnath <158467163+Rajdeep522@users.noreply.github.com>
2025-06-30 00:18:35 +0530

    Update Todo.jsx

--- Todo.jsx
+++ Todo.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import {v4 as uuidv4} from 'uuid';
 export default function Todo(){
-    const[taskList,setTaskList]=useState([{tk: "Sample Task",id: uuidv4()}]);
+    const[taskList,setTaskList]=useState([{tk: "Sample Task",id: uuidv4(),isDone: false}]);
     const[task,settask]=useState("");
 
     let addtask=(()=>{
         setTaskList((pretask)=>{
-            return [...pretask,{tk: task, id: uuidv4()}]});
+            return [...pretask,{tk: task, id: uuidv4(), isDone: false}]});
         settask("");
     });
 
@@ -20,29 +20,68 @@ export default function Todo(){
 
     });
 
+    let updateUppercaseALL = (() => {
+        setTaskList((prevtask) =>
+            prevtask.map((tk) => {
+                return { ...tk, tk: tk.tk.toUpperCase() }
+            })
+        );
+    });
+
+    let updateone=((id)=>{
+        setTaskList(
+            (prevtask)=>
+                prevtask.map(
+                    (tk)=>{
+                        if(tk.id===id){
+                            return{
+                                ...tk, tk: tk.tk.toUpperCase()
+                            }
+                        }else{
+                            return tk;
+                        }
+                    }
+                )
+        )
+    })
+
+    let Done=(
+        (id)=>{
+
+        }
+    )
+
+    let DoneALL = (() => {
+
+    });
    
     return (
         <>
             <div>
                 <h4>Todo List</h4>
-                <input type="text" onChange={updateTask} style={{border: "1px solid black"}} placeholder="Please enter the task" value={task} />
+                <input type="text" onChange={updateTask}  style={{border: "1px solid black"}} placeholder="Please enter the task" value={task} required />
                 <button type="submit" onClick={addtask}>Add</button>
                 <br />
                 <br />
+                <hr />
                 <ul>
                     {taskList.map((tk) => (
-                        <li key={tk.id}>
-                            <span>{tk.tk}</span>
+                        <li key={tk.id}> 
+                            <br />
+                            <span className="line-through">{tk.tk}</span>
                              &nbsp;&nbsp;
                             <button className="bg-red-600" onClick={()=>deleteTask(tk.id)}>delete</button> &nbsp; &nbsp;
-                            <button className="bg-blue-500">edit</button>
+                            <button className="bg-blue-500" onClick={()=>updateone(tk.id)}>edit</button>&nbsp; &nbsp;
+                            <button className="bg-green-500" onClick={()=>Done(tk.id)}>Mark as Done</button>&nbsp; &nbsp;
                             
                         </li>
                         ))}
-                        <br />
-                        <button className="bg-yellow-400">edit all</button>
-                       
                 </ul>
+                <br />
+                <button className="bg-yellow-400" onClick={updateUppercaseALL}>edit all</button> <br /> <br />
+                <button className="bg-green-800 text-white" onClick={DoneALL}>edit all</button>
+                       
+                
             </div>
         </>
     )
